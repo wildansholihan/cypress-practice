731179804941d2854ea707b9fe63dfabc2657d9c
ekosy9 <wildansholihan0@gmail.com>
2025-02-18 10:38:52 +0700

    dalaj

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,17 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    // To display small circular charts regarding test results
+    charts: true,
+    // Generate JSON file to create custom reports
+    json: true,
+    // Customize the directory in which reports are saved
+    reportsDir: 'reports/your-reports-folder',
+    // Customize the report file name
+    reportFilename: 'my-report',
+    // Generate new report file or overwrite the a single file
+    overwrite: false
+  }
 });
